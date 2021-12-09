80b5fb153d6b8dc078ce2f458d02733795738c39
Markus Ijäs <markus.ijas@outlook.com>
2021-12-09 10:07:26 +0200

    Adds boilerplate components

---
+++ app/src/components/VehicleSelectionRadio.js
@@ -0,0 +1,14 @@
+// SPDX-FileCopyrightText: 2021 Hannu Santti
+// SPDX-FileCopyrightText: 2021 Jani Lehtinen
+// SPDX-FileCopyrightText: 2021 Markus Ijäs
+// SPDX-FileCopyrightText: 2021 Markus Murto
+// SPDX-FileCopyrightText: 2021 Susanna Mikola
+//
+// SPDX-License-Identifier: MIT
+
+function VehicleSelectionRadio() {
+  return (
+  );
+}
+
+export default VehicleSelectionRadio;
