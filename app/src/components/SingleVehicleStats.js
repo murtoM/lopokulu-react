db11ba0bb5432389f5929d1809987e4680224fb6
Markus Murto <markus@murto.eu>
2021-12-11 21:36:29 +0200

    Implement SingleVehicleStats, resolves #22, #8, #9, #10, #11 and #12

--- app/src/components/SingleVehicleStats.js
+++ app/src/components/SingleVehicleStats.js
@@ -6,10 +6,64 @@
 //
 // SPDX-License-Identifier: MIT
 
-function SingleVehicleStats() {
+import { roundToPrecision } from "./helpers";
+
+function SingleVehicleStats({ fillups }) {
+  let { sums, averages } = calculate(fillups);
   return (
-    <li>Stat Row</li>
+    <div className="grid" id="single-vehicle-stats">
+      <div id="single-vehicle-totals">
+        <strong>Total:</strong>
+        <li>{roundToPrecision(sums.litres, 1)} litres</li>
+        <li>{roundToPrecision(sums.cost, 1)} € </li>
+        <li>{roundToPrecision(sums.distance, 1)} km</li>
+      </div>
+      <div id="single-vehicle-averages">
+        <strong>Average:</strong>
+        <li>{roundToPrecision(averages.cost, 1)} € / 100km</li>
+        <li>{roundToPrecision(averages.litres, 1)} l / 100km
+        </li>
+      </div>
+    </div>
   );
+}
+
+function calculate(fillups) {
+  let sums = {
+    litres: 0,
+    cost: 0,
+    distance: 0,
+  };
+  let averages = {
+    litres: 0,
+    cost: 0,
+  };
+
+  let { litres, cost, distance } = sumFillups(fillups);
+  sums.litres = litres;
+  sums.cost = cost;
+  sums.distance = distance;
+
+  if (sums.distance > 0) {
+    averages.litres = (sums.litres / sums.distance) * 100;
+    averages.cost = (sums.cost / sums.distance) * 100;
+  }
+
+  return { sums, averages };
+}
+
+function sumFillups(fillups) {
+  let litres = 0;
+  let cost = 0;
+  let distance = 0;
+
+  fillups.forEach((fillup) => {
+    litres += fillup.litres;
+    distance += fillup.distance_driven;
+    cost += fillup.total_price;
+  });
+
+  return { litres, cost, distance };
 }
 
 export default SingleVehicleStats;
