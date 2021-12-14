fea568001ae56fe2353a6d0c345303a5bc017c77
Markus Ijäs <markus.ijas@outlook.com>
2021-12-14 10:01:22 +0200

    Adds licensing information to helpers.js [#1]

--- app/src/components/helpers.js
+++ app/src/components/helpers.js
@@ -1,3 +1,11 @@
+// SPDX-FileCopyrightText: 2021 Hannu Santti
+// SPDX-FileCopyrightText: 2021 Jani Lehtinen
+// SPDX-FileCopyrightText: 2021 Markus Ijäs
+// SPDX-FileCopyrightText: 2021 Markus Murto
+// SPDX-FileCopyrightText: 2021 Susanna Mikola
+//
+// SPDX-License-Identifier: MIT
+
 /**
  * Precision rounding helper
  *
