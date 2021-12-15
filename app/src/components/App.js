be40fe76fa789a7450d25f76f27025bfa85d8f66
Markus Ijäs <markus.ijas@outlook.com>
2021-12-15 15:03:26 +0200

    Implements localstorage [#25]

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -5,17 +5,26 @@
 //
 // SPDX-License-Identifier: MIT
 
-import React, {useReducer} from "react";
-import Modal from "./Modal";
-import VehicleList from "./VehicleList";
-import GlobalStats from "./GlobalStats";
-import vehicles from "../vehicle-data.json";
-import { FillupDataContext, ModalContext, initialModalState } from "./contexts";
-import { vehicleReducer, modalReducer } from "./reducers";
+import React, { useReducer, useEffect } from 'react';
+import Modal from './Modal';
+import VehicleList from './VehicleList';
+import GlobalStats from './GlobalStats';
+import vehicles from '../vehicle-data.json';
+import { FillupDataContext, ModalContext, initialModalState } from './contexts';
+import { vehicleReducer, modalReducer } from './reducers';
 
 function App() {
+  const raw_localstorage = localStorage.getItem('vehicles');
+
   const [modalState, modalDispatch] = useReducer(modalReducer, initialModalState);
-  const [vehicleState, vehicleDispatch] = useReducer(vehicleReducer, vehicles);
+  const [vehicleState, vehicleDispatch] = useReducer(
+    vehicleReducer,
+    JSON.parse(raw_localstorage) || vehicles,
+  );
+
+  useEffect(() => {
+    localStorage.setItem('vehicles', JSON.stringify(vehicleState));
+  }, [vehicleState]);
 
   return (
     <FillupDataContext.Provider value={{vehicleState, vehicleDispatch}}>
